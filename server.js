580f330c206cd1f2c5c7e34d444d4894fdbda1f6
Nicholas King <40317748+Kingdroid1@users.noreply.github.com>
2019-07-15 03:43:41 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-// const jwt = require('./helpers/jwt');
 const errorHandler = require('./helpers/error-handler');
 
 //use mongoose library to set up the database connection with MongoDB. We can also use Mongoose to save the data in the database using Mongoose ORM.
@@ -14,11 +13,8 @@ require('./models/users');
  
 //controllers for models
 const rtsIndex = require('./routes/index');
-const contactRoute = require('./routes/contact-route');
-const electricityRoute = require('./routes/electricity-route');
 const paymentRoute = require('./routes/payment-route');
 const transactionRoute = require('./routes/trasaction-route');
-// const discoRoute = require('./routes/tr-route');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.DB, { useNewUrlParser: true }).then(
@@ -31,13 +27,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
-// use JWT auth to secure the api
-// app.use(jwt());
-
 // api routes
-app.use('/contact', contactRoute);
 app.use('/api', rtsIndex);
-app.use('', electricityRoute);
 app.use('/payment', paymentRoute);
 app.use('/transaction', transactionRoute);
 
@@ -45,7 +36,7 @@ app.use('/transaction', transactionRoute);
 app.use(errorHandler);
 
 // start server
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
 const server = app.listen(port, function(){
  console.log('Listening on port ' + port);
